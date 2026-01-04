94dba09e3dd0004db807ff233e39000e9a332ed4
mcdonaldmark <markamcdonald7@gmail.com>
2026-01-04 14:57:13 -0700

    sync

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -7,6 +7,26 @@ export default function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
+  
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false }
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
   }
 
   return (
@@ -20,21 +40,16 @@ export default function App() {
     </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
       </ul>
-      </>
+    </>
   )
 }
